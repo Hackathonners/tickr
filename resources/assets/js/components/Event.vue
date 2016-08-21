42a7fc0e3dffa3f7bced44b0209262510a6d94b7
Francisco Neves <fntneves@gmail.com>
2016-08-21 21:22:30 +0100

    Working on events show

--- resources/assets/js/components/Event.vue
+++ resources/assets/js/components/Event.vue
@@ -2,15 +2,19 @@
   <div class="row">
     <div class="col-md-12">
       <h3 class="page-title">{{ event.title }}</h3>
+      <div class="text-muted">{{ event.place }}</div>
+      <div class="text-muted">{{ event.start_at | date_range event.end_at }}</div>
     </div>
   </div>
 
-  <pre>{{ event |json }}</pre>
-  <create-registration :registrations.sync="registrations" :event="event" :registration-types="event.registration_types.data"></create-registration>
+  <!-- <pre>{{ event | json }}</pre> -->
+  <!-- <create-registration :registrations.sync="registrations" :event="event" :registration-types="event.registration_types.data"></create-registration> -->
 </template>
 
 <script>
+  import moment from 'moment';
   import CreateRegistration from './Forms/CreateRegistration.vue';
+  import '../filters/Date';
   export default {
     data() {
       return {
--- resources/assets/js/filters/Date.js
+++ resources/assets/js/filters/Date.js
@@ -14,3 +14,28 @@ Vue.filter('human_diff', (date, suffix = false, inFormat = "YYYY-MM-DD HH:mm:ss"
 
   return moment(date, inFormat).fromNow(suffix);
 })
+
+Vue.filter('date_range', (dateFrom, dateTo, format = "YYYY-MM-DD HH:mm:ss") => {
+  let from = moment(dateFrom, format);
+  let to = moment(dateTo, format);
+
+  if(!from.isValid() || !to.isValid())
+    return;
+
+  if(to.isSame(from, 'day'))
+    return from.format('dddd, D MMMM YYYY') +
+      ' das ' +
+      from.format('HH:mm') +
+      ' às ' +
+      to.format('HH:mm');
+
+ return from.format('dddd, D MMMM YYYY') +
+      ' às ' +
+      from.format('HH:mm') +
+      ' — ' +
+      to.format('dddd, D MMMM YYYY') +
+      ' às ' +
+      to.format('HH:mm');
+
+  return moment(date, inFormat).fromNow(suffix);
+})
