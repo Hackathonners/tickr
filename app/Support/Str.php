ac1e1ff375c0665051873ac1f0713b5c5246f69e
Francisco Neves <fntneves@gmail.com>
2016-09-08 12:37:09 +0100

    Add searchable mutator

---
+++ app/Support/Str.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Support;
+
+class Str extends \Illuminate\Support\Str
+{
+    /**
+     * Returns the searchable string.
+     *
+     * @param  mixed $array
+     * @param bool $trim
+     * @return mixed $array
+     */
+    public static function searchable($string, $replace = false, $min = 3, $charReplace = '%')
+    {
+        // Remove duplicated spaces
+        $string = preg_replace('/\s+/', ' ', self::lower(self::ascii(trim($string))));
+        $splitString = explode(' ', $string);
+
+        $terms = array_filter($splitString, function($term) use ($min) {
+            return strlen($term) >= $min;
+        });
+
+        // There are terms with 3 chars, concatenate them and use it as search string.
+        if(count($terms) > 0) {
+            return implode($replace ? $charReplace : ' ', $terms);
+        }
+
+        return $string;
+    }
+}
---
+++ tests/Unit/StrTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Support\Str;
+
+class StrTest extends TestCase
+{
+    public function testSearchableString()
+    {
+        $query1 = 'wõrd';
+        $query2 = 'néw string';
+        $query3 = 'a stríng';
+        $query4 = '   a string   ';
+        $query5 = '   a big string   ';
+        $query6 = '   ';
+        $query7 = '';
+
+        $this->assertEquals('word', Str::searchable($query1));
+        $this->assertEquals('word', Str::searchable($query1, true, 10));
+
+        $this->assertEquals('new string', Str::searchable($query2));
+        $this->assertEquals('new%string', Str::searchable($query2, true));
+        $this->assertEquals('new%string', Str::searchable($query2, true));
+
+        $this->assertEquals('string', Str::searchable($query3));
+        $this->assertEquals('a string', Str::searchable($query3, false, 1));
+        $this->assertEquals('a%string', Str::searchable($query3, true, 1));
+
+        $this->assertEquals('big string', Str::searchable($query5));
+
+        $this->assertEquals('', Str::searchable($query6));
+        $this->assertEquals('', Str::searchable($query7));
+    }
+}
