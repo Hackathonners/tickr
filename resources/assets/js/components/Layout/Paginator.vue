bf38bd4f490312b6107366bdc7ce0ec6da28917e
Francisco Neves <fntneves@gmail.com>
2016-09-16 17:22:03 +0100

    Add paginator component

---
+++ resources/assets/js/components/Layout/Paginator.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav aria-label="...">
+    <ul class="pager">
+      <li :class="{ disabled: pagination.current_page == 1 }"><a href="#" @click.prevent="goToPage(pagination.current_page-1)">Previous</a></li>
+      <li :class="{ disabled: pagination.current_page == pagination.total_pages }"><a href="#" @click.prevent="goToPage(pagination.current_page+1)">Next</a></li>
+    </ul>
+  </nav>
+</template>
+<script>
+  export default {
+    props: {
+      pagination: {
+        type: Object,
+        required: true
+      },
+      callback: {
+        type: Function,
+        required: true
+      },
+    },
+    methods: {
+      goToPage(page = 1) {
+        this.$set('pagination.current_page', page);
+        this.callback(page);
+      }
+    }
+  }
+</script>
