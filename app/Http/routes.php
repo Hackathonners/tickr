4685c54d6798113d5b0335a344bf46654fac9b0b
Diogo Couto <diogo2couto@gmail.com>
2016-10-05 20:49:42 +0100

    Remove authentication from routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Auth::login(\App\Karina\User::first());
+//Auth::login(\App\Karina\User::first());
 
 Route::group(['prefix' => 'api/v1'], function () {
     Route::resource('events', 'Api\EventsController');
