07913558f759885fd3e3a75e0570d824c0ba27ea
Francisco Neves <fntneves@gmail.com>
2016-10-23 16:02:38 +0100

    Add paginator component

---
+++ resources/assets/js/app/components/paginator/paginator.js
@@ -0,0 +1,39 @@
+/* ============
+ * Paginator Component
+ * ============
+ *
+ * Paginates data and set up links to previous and next pages.
+ */
+
+export default {
+  props: {
+    pagination: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    nextPage () {
+      return this.buildQuery(this.pagination.current_page + 1)
+    },
+    prevPage() {
+      return this.buildQuery(this.pagination.current_page - 1)
+    },
+    buildQuery (page) {
+      return {
+        query: Object.assign({}, this.$store.state.route.query, { page })
+      }
+    }
+  },
+  computed: {
+    singlePage () {
+      return !this.pagination.total_pages || this.pagination.total_pages < 2
+    },
+    onFirstPage () {
+      return this.pagination.current_page === 1
+    },
+    onLastPage () {
+      return this.pagination.current_page === this.pagination.total_pages
+    },
+  }
+}
---
+++ resources/assets/js/app/components/paginator/paginator.vue
@@ -0,0 +1,2 @@
+<template src="./paginator.html"></template>
+<script src="./paginator.js"></script>
