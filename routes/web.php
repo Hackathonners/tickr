fddb0f49100833b37d2077034b4a270c15333c47
Francisco Neves <fntneves@gmail.com>
2016-10-26 19:39:58 +0100

    Fix typo in Router class

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,19 @@
 */
 
 // Auth Routes
-Router::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Router::post('login', 'Auth\LoginController@login');
-Router::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
-// Router::get('register', 'Auth\RegisterController@showRegistrationForm');
-// Router::post('register', 'Auth\RegisterController@register');
+// Route::get('register', 'Auth\RegisterController@showRegistrationForm');
+// Route::post('register', 'Auth\RegisterController@register');
 
 // Password Reset Routes...
-Router::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-Router::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-Router::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
-Router::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('{all}', function () {
     return view('app');
