af3f0d44fb9972e482f98ae1241f4bb273501e15
Francisco Neves <fntneves@gmail.com>
2016-10-26 19:39:16 +0100

    Disable registration routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,20 @@
 |
 */
 
-Auth::routes();
+// Auth Routes
+Router::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Router::post('login', 'Auth\LoginController@login');
+Router::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+// Router::get('register', 'Auth\RegisterController@showRegistrationForm');
+// Router::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Router::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Router::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Router::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Router::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('{all}', function () {
     return view('app');
