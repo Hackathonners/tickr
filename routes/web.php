f3e3b1c981077c7dcc2d9e2ffae60c868be0b6e3
Francisco Neves <fntneves@gmail.com>
2016-10-27 09:19:33 +0100

    Enable registration routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
-// Route::get('register', 'Auth\RegisterController@showRegistrationForm');
-// Route::post('register', 'Auth\RegisterController@register');
+Route::get('register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('register', 'Auth\RegisterController@register');
 
 // Password Reset Routes...
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
