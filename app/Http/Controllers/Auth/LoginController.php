c3e3c242ea34e61da11c75b4b906430f4e682b93
Francisco Neves <fntneves@gmail.com>
2016-10-28 11:27:47 +0100

    Change redirect route after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/events';
 
     /**
      * Create a new controller instance.
