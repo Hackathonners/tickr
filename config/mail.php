6e579fe16e07b43c2f067fc895c435e54b5e7955
Francisco Neves <fntneves@gmail.com>
2016-12-06 12:29:30 +0000

    Put email back

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_EMAIL', 'contact@hackathonners.org'),
+        'address' => env('MAIL_FROM_EMAIL', 'hackathonners@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Tickr | Hackathonners'),
     ],
 
