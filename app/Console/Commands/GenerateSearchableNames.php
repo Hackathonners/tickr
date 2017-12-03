f962b06528d0ce8e77129e46b8d3c506ed174be1
Francisco Neves <fntneves@gmail.com>
2017-12-03 22:17:22 +0000

    Generate ASCII search names

--- app/Console/Commands/GenerateSearchableNames.php
+++ app/Console/Commands/GenerateSearchableNames.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Karina\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -39,7 +40,11 @@ class GenerateSearchableNames extends Command
     public function handle()
     {
         DB::transaction(function () {
-            DB::table('users')->update(['name_search' => DB::raw('LOWER(name)')]);
+            $users = User::all();
+            foreach($users as $user) {
+                $user->name = $user->name;
+                $user->save();
+            }
         });
     }
 }
