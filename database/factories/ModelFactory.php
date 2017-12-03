6638643d64689a783d6740e109080db7e5927b47
Francisco Neves <fntneves@gmail.com>
2017-12-03 21:47:26 +0000

    Fix factories and test against PHP 7.1

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -14,7 +14,7 @@
 $factory->define(App\Karina\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->safeEmail,
+        'email' => $faker->unique()->safeEmail,
         'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
